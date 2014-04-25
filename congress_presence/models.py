bf719342c4e3b98cb2f30167461c3793b69a31c5
Marios Kogias <marioskogias@grnet.gr>
2014-04-26 00:00:16 +0300

    Add the basic model

--- congress_presence/models.py
+++ congress_presence/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Participant(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    lc =  models.CharField(max_length=30)
+    here = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s %s from LC %s" % (self.first_name, self.last_name, self.lc)
