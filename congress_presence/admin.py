4c515764c755011b6f808da3dc756701333c05da
Marios Kogias <marioskogias@grnet.gr>
2014-04-26 02:12:14 +0300

    Add admin panel action to mark all participants as missing

---
+++ congress_presence/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from models import Participant
+
+def make_absent(modeladmin, request, queryset):
+    queryset.update(here=False)
+make_absent.short_description = "Mark selected participants as missing"
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'lc']
+    ordering = ['lc']
+    actions = [make_absent]
+
+admin.site.register(Participant, ParticipantAdmin)
